6ad41a083f9d71ad65f38cc87362bd5833ed9c18
Tana Franko <tlfranko@gmail.com>
2025-11-20 16:59:57 -0800

    Confirmed local build success

---
+++ public/searchindex.en.js
@@ -0,0 +1,58 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Portfolio Home \u003e Who I Am",
+    "content": "About Tana\rThanks for checking out my writing portfolio.\nMy technical writing career has included quite a few different authoring tools and technologies.\nI’m building this porfolio using Hugo.",
+    "description": "About Tana\rThanks for checking out my writing portfolio.\nMy technical writing career has included quite a few different authoring tools and technologies.\nI’m building this porfolio using Hugo.",
+    "tags": [],
+    "title": "About Tana",
+    "uri": "/technical-writing-portfolio/introduction/whoami/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Welcome to my technical writing portfolio.",
+    "description": "Welcome to my technical writing portfolio.",
+    "tags": [],
+    "title": "Portfolio Home",
+    "uri": "/technical-writing-portfolio/index.html"
+  },
+  {
+    "breadcrumb": "Portfolio Home \u003e Writing Samples",
+    "content": "This is a sample piece of technical documentation.\nI can include tables, callouts, images, links, and more.",
+    "description": "This is a sample piece of technical documentation.\nI can include tables, callouts, images, links, and more.",
+    "tags": [],
+    "title": "Sample 1",
+    "uri": "/technical-writing-portfolio/writing-samples/sample1/index.html"
+  },
+  {
+    "breadcrumb": "Portfolio Home",
+    "content": "Here is some text about me.",
+    "description": "Here is some text about me.",
+    "tags": [],
+    "title": "Who I Am",
+    "uri": "/technical-writing-portfolio/introduction/index.html"
+  },
+  {
+    "breadcrumb": "Portfolio Home",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Writing Samples",
+    "uri": "/technical-writing-portfolio/writing-samples/index.html"
+  },
+  {
+    "breadcrumb": "Portfolio Home",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/technical-writing-portfolio/categories/index.html"
+  },
+  {
+    "breadcrumb": "Portfolio Home",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/technical-writing-portfolio/tags/index.html"
+  }
+]
